b16a87220de9cb99787c10f8f65aa26196162d6b
S.Miah <serhanmiah@icloud.com>
2025-05-18 01:59:40 +0100

    start of queries.sql, showing all table using .mode box for easier sql visual

--- queries.sql
+++ queries.sql
@@ -1,1 +1,16 @@
 -- In this SQL file, write (and comment!) the typical SQL queries users will run on your database
+
+  -- Run SQL queries to inspect data in your tables:
+-- 
+.mode box
+
+-- sql - shows all the table
+  SELECT * FROM Users;
+  SELECT * FROM Categories;
+  SELECT * FROM Transactions;
+  SELECT * FROM Budgets;
+  SELECT * FROM Contracts;
+  SELECT * FROM recurring_transactions;
+  SELECT * FROM Goals;
+  SELECT * FROM payment_methods;
+
